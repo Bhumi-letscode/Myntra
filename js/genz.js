97e62c089b675d00dad9125334d29c604d9f6cbf
bhumi-letscode <spk2bhumi@gmail.com>
2024-06-20 16:16:28 +0530

    hihli

--- js/genz.js
+++ js/genz.js
@@ -30,7 +30,7 @@ function showSlides() {
 
 
 function dropDown1() {
-    let content = document.querySelector(".dropdown-content1");
+    let content = document.querySelector(".dropdown-content2");
 
     content.addEventListener("mouseover", function () {
         let menu = document.querySelector(".dropdown-menu1");
@@ -52,117 +52,4 @@ function dropDown1() {
 }
 dropDown1();
 
-function dropDown2() {
-    let content = document.querySelector(".dropdown-content2");
 
-    content.addEventListener("mouseover", function () {
-        let menu = document.querySelector(".dropdown-menu2");
-        menu.style.display = "block";
-    });
-
-    let menu = document.querySelector(".dropdown-menu2");
-    menu.addEventListener("mouseover", function () {
-        menu.style.display = "block";
-        menu.addEventListener("mouseout", function () {
-            menu.style.display = "none";
-        });
-    });
-
-    content.addEventListener("mouseout", function () {
-        let menu = document.querySelector(".dropdown-menu2");
-        menu.style.display = "none";
-    });
-}
-dropDown2();
-
-function dropDown3() {
-    let content = document.querySelector(".dropdown-content3");
-
-    content.addEventListener("mouseover", function () {
-        let menu = document.querySelector(".dropdown-menu3");
-        menu.style.display = "block";
-    });
-
-    let menu = document.querySelector(".dropdown-menu3");
-    menu.addEventListener("mouseover", function () {
-        menu.style.display = "block";
-        menu.addEventListener("mouseout", function () {
-            menu.style.display = "none";
-        });
-    });
-
-    content.addEventListener("mouseout", function () {
-        let menu = document.querySelector(".dropdown-menu3");
-        menu.style.display = "none";
-    });
-}
-dropDown3();
-
-function dropDown4() {
-    let content = document.querySelector(".dropdown-content4");
-
-    content.addEventListener("mouseover", function () {
-        let menu = document.querySelector(".dropdown-menu4");
-        menu.style.display = "block";
-    });
-
-    let menu = document.querySelector(".dropdown-menu4");
-    menu.addEventListener("mouseover", function () {
-        menu.style.display = "block";
-        menu.addEventListener("mouseout", function () {
-            menu.style.display = "none";
-        });
-    });
-
-    content.addEventListener("mouseout", function () {
-        let menu = document.querySelector(".dropdown-menu4");
-        menu.style.display = "none";
-    });
-}
-dropDown4();
-
-function dropDown5() {
-    let content = document.querySelector(".dropdown-content5");
-
-    content.addEventListener("mouseover", function () {
-        let menu = document.querySelector(".dropdown-menu5");
-        menu.style.display = "block";
-    });
-
-    let menu = document.querySelector(".dropdown-menu5");
-    menu.addEventListener("mouseover", function () {
-        menu.style.display = "block";
-        menu.addEventListener("mouseout", function () {
-            menu.style.display = "none";
-        });
-    });
-
-    content.addEventListener("mouseout", function () {
-        let menu = document.querySelector(".dropdown-menu5");
-        menu.style.display = "none";
-    });
-}
-dropDown5();
-
-function dropDown6() {
-    let content = document.querySelector(".dropdown-content6");
-
-    content.addEventListener("mouseover", function () {
-        let menu = document.querySelector(".dropdown-menu6");
-        menu.style.display = "block";
-    });
-
-    let menu = document.querySelector(".dropdown-menu6");
-    menu.addEventListener("mouseover", function () {
-        menu.style.display = "block";
-        menu.addEventListener("mouseout", function () {
-            menu.style.display = "none";
-        });
-    });
-
-    content.addEventListener("mouseout", function () {
-        let menu = document.querySelector(".dropdown-menu6");
-        menu.style.display = "none";
-    });
-}
-dropDown6();
